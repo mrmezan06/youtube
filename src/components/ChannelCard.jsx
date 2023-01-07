1571faf4ebd383df92c2e510dbb734a64ba87f4d
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-01-07 14:51:43 +0600

    Till Channel Card

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,62 @@
+import { CheckCircle } from '@mui/icons-material';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { demoProfilePicture } from '../utils/constants';
+
+const ChannelCard = ({ channelDetail }) => {
+  return (
+    <Box
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: { xs: '356px', md: '320px' },
+        height: '326px',
+        margin: 'auto',
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            textAlign: 'center',
+            color: '#fff',
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thumbnails?.medium?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              width: '180px',
+              height: '180px',
+              borderRadius: '50%',
+              mb: 2,
+              border: '1px solid #e3e3e3',
+            }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
+          </Typography>
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString()}{' '}
+              Subscribers
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
+  );
+};
+
+export default ChannelCard;
